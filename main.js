473539d8ad4d83b3cb1ccb3cea76bf2a09a76b43
edwinisnow <edwin04@gmail.com>
2020-04-26 00:34:58 +0100

    Variable Types

--- main.js
+++ main.js
@@ -7,3 +7,47 @@ var x = 20;
 var y = 10;
 var sum;
 var title = 'Application Title';
+// Variable Types
+var isBeginner = true;
+var total = 0;
+var name = 'John';
+var sentence = "My name is " + name + " \nI am a beginner in Typescript";
+console.log("Log: -----> : sentence", sentence);
+// Null
+var n = null;
+// Undefined
+var u = undefined;
+// Boolean
+var isNew = null;
+var myName = undefined;
+var list1 = [1, 2, 3];
+// or
+var list2 = [1, 2, 3];
+// Tuple
+var person1 = ['John Doe', 23];
+// Enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["Blue"] = 7] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Green;
+console.log("Log: -----> : c", c);
+// Any
+var randomValue = 10;
+randomValue = true;
+randomValue = 'John Doe';
+// Unknown
+// let myVariable: unknown = 10;
+// function hasName(obj: any): obj is { name: string } {
+//     return !!obj && typeof obj === 'object' && "name" in obj
+// }
+// if (hasName(myVariable)) {
+//     console.log("Log: -----> : myVariable", myVariable)
+// }
+// (myVariable as string).toUpperCase();
+// union of types
+var multiType;
+multiType = 20;
+multiType = false;
--- main.ts
+++ main.ts
@@ -9,4 +9,46 @@ const y = 10;
 let sum;
 const title = 'Application Title'
 
+// Variable Types
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'John'
+let sentence: string = `My name is ${name} 
+I am a beginner in Typescript`
+console.log("Log: -----> : sentence", sentence)
+// Null
+let n: null = null
+// Undefined
+let u: undefined = undefined
+// Boolean
+let isNew: Boolean = null
+let myName: string = undefined
 
+let list1: number[] = [1, 2, 3]
+// or
+let list2: Array<number> = [1, 2, 3]
+// Tuple
+let person1: [string, number] = ['John Doe', 23]
+// Enum
+enum Color { Red = 5, Green, Blue }
+let c: Color = Color.Green;
+console.log("Log: -----> : c", c)
+// Any
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'John Doe'
+// Unknown
+// let myVariable: unknown = 10;
+// function hasName(obj: any): obj is { name: string } {
+//     return !!obj && typeof obj === 'object' && "name" in obj
+// }
+// if (hasName(myVariable)) {
+//     console.log("Log: -----> : myVariable", myVariable)
+// }
+
+// (myVariable as string).toUpperCase();
+
+// union of types
+let multiType: number | boolean;
+multiType = 20;
+multiType = false;
